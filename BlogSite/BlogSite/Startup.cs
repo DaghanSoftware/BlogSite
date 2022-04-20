e437ddb49e5a1cf35aadb1662fb04b298b5ac252
Semih Dağhan <daghanex@hotmail.com>
2022-04-20 15:13:12 +0300

    404 Ekranı tasarlandı ve startup a urlde yanlış adrese gidildiğinde yönlendirme yapıldı

--- BlogSite/BlogSite/Startup.cs
+++ BlogSite/BlogSite/Startup.cs
@@ -39,6 +39,7 @@ namespace BlogSite
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");//Olmayan bir url e gidildiðinde  uyarý verilmesi için kullanýlan parametre.
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
