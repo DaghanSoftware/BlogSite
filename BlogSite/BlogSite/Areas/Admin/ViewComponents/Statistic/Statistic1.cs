47d4c13a7b60fe39d7a6dcc5b14dca4c04bcb35f
Semih Dağhan <daghanex@hotmail.com>
2022-05-24 13:54:17 +0300

    İstatistikler ekleniyor
    ToplamBlogSayisi
    ContactCount
    ToplamYorumSayisi

---
+++ BlogSite/BlogSite/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogSite.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.ToplamBlogSayisi = bm.GetList().Count();
+            ViewBag.ContactCount = c.Contacts.Count();
+            ViewBag.ToplamYorumSayisi = c.Comments.Count();
+            return View();
+        }
+    }
+}
