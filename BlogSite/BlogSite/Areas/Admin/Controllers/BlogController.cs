66c23fbc4c0c3611b44d8546ba128e4a59ce8b85
Semih Dağhan <daghanex@hotmail.com>
2022-05-24 13:06:12 +0300

    Widget Sayfasını Oluşturdum

--- BlogSite/BlogSite/Areas/Admin/Controllers/BlogController.cs
+++ BlogSite/BlogSite/Areas/Admin/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using BlogSite.Areas.Admin.Models;
 using ClosedXML.Excel;
+using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -49,10 +50,55 @@ namespace BlogSite.Areas.Admin.Controllers
             };
             return bm;
         }
-        [AllowAnonymous]
         public IActionResult BlogListExcel()
         {
             return View();
         }
+
+        #region Dinamik Olarak Veritabanından Gelen Değerleri Excel Tablosun Yazdırma
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in BlogTitlelist())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+        }
+
+        public List<BlogModel2> BlogTitlelist()
+        {
+            List<BlogModel2> bm = new List<BlogModel2>();
+            using(var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel2
+                {
+                    ID=x.BlogID,
+                    BlogName=x.BlogTitle
+                }).ToList();
+            }
+            return bm;
+        }
+
+        public IActionResult BlogTitleListExcel()
+        {
+            return View();
+        }
+        #endregion
     }
 }
