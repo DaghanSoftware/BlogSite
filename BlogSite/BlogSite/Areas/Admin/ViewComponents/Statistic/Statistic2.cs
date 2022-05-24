4ad781f761f2e8fbfb9bd854a296b4b57fc4b97c
Semih Dağhan <daghanex@hotmail.com>
2022-05-24 17:37:15 +0300

    İstatistik ile iglili eklemeler yapıldı
    SonEklenenBlogAdı

---
+++ BlogSite/BlogSite/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogSite.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            @ViewBag.SonEklenenBlogAdı = c.Blogs.OrderByDescending(x=>x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
