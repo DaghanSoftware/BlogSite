b511779f00d2d04ab6c37d7a87eeba757c47bc6b
Semih Dağhan <daghanex@hotmail.com>
2022-06-02 21:11:29 +0300

    Api Test işlemleri yapıldı

---
+++ BlogSite/BlogSite/Areas/Admin/Controllers/ApiTestController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlogSite.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Area("Admin")]
+    public class ApiTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44301/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+    }
+    public class Class1
+    {
+        public int WriterID { get; set; }
+
+        public string WriterName { get; set; }
+
+        public string WriterAbout { get; set; }
+
+        public string WriterImage { get; set; }
+
+        public string WriterMail { get; set; }
+
+        public string WriterPassword { get; set; }
+
+        public bool WriterStatus { get; set; }
+
+        public int CityId { get; set; }
+    }
+}
