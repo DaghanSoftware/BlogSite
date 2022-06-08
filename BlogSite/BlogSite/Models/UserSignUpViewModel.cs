d66e5a6ea20b1042882be465e70c2b0da72d667c
Semih Dağhan <daghanex@hotmail.com>
2022-06-08 22:57:01 +0300

    Identity Kullanarak Kayıt Olma İşlemleri

---
+++ BlogSite/BlogSite/Models/UserSignUpViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogSite.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage ="Lütfen AdSoyad Giriniz")]
+        public string NameSurname { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Lütfen şifre Giriniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrar")]
+        [Compare("Password",ErrorMessage ="Şifreler Uyuşmuyor!")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Mail Adresi")]
+        [Required(ErrorMessage = "Lütfen mail giriniz ")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Kullanıcı ADI")]
+        [Required(ErrorMessage = "Lütfen Kullanıcı adını Giriniz")]
+        public string UserName { get; set; }
+
+
+
+    }
+}
