9acb73b0618c0bc6145e00c8ddff6750cb09edb1
Semih Dağhan <daghanex@hotmail.com>
2022-06-13 01:29:50 +0300

    Yazar panelinde profil erkanında revizeler
    Güncelleme işlemi yaparken identity updateasync üzerinden güncelleme işlemi yapılacak şekilde revize edildi

--- BlogSite/BlogSite/Controllers/DashBoardController.cs
+++ BlogSite/BlogSite/Controllers/DashBoardController.cs
@@ -10,11 +10,16 @@ namespace BlogSite.Controllers
 {
     public class DashBoardController : Controller
     {
+        Context c = new Context();
         public IActionResult Index()
         {
-            Context c = new Context();
+
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var WriterId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+       
             ViewBag.ToplamBlogSayisi = c.Blogs.Count().ToString();
-            ViewBag.YazarınBlogSayısı = c.Blogs.Where(x=>x.WriterID==1).Count().ToString();
+            ViewBag.YazarınBlogSayısı = c.Blogs.Where(x=>x.WriterID== WriterId).Count().ToString();
             ViewBag.ToplamKategoriSayısı = c.Blogs.Count().ToString();
             return View();
         }
--- BlogSite/BlogSite/Controllers/WriterController.cs
+++ BlogSite/BlogSite/Controllers/WriterController.cs
@@ -6,6 +6,7 @@ using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
@@ -22,6 +23,13 @@ namespace BlogSite.Controllers
         //[Authorize]
         //[AllowAnonymous]
         WriterManager wm = new WriterManager(new EfWriterRepository());
+        private readonly UserManager<AppUser> _userManager;
+
+        public WriterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
@@ -59,40 +67,39 @@ namespace BlogSite.Controllers
             return PartialView();
         }
         [HttpGet]
-        public IActionResult WriterEditProfile()
+        public async Task<IActionResult>  WriterEditProfile()
         {
-            var usermail = User.Identity.Name;
-            Context c = new Context();
-            var WriterId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            //Context c = new Context();
+            //var username = User.Identity.Name;
 
-            ViewBag.Cities = new SelectList(cm.GetList().ToList(), "CityId", "CityName");
-            var writervalues = wm.GetById(WriterId);
-            return View(writervalues);
+            //var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            //UserManager userManager = new UserManager(new EfUserRepository());
+
+            //var WriterId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+
+            //ViewBag.Cities = new SelectList(cm.GetList().ToList(), "CityId", "CityName");
+            //var writervalues = wm.GetById(WriterId);
+
+            //var id = c.Users.Where(x => x.Email == usermail).Select(y => y.Id).FirstOrDefault();
+            //var values = userManager.GetById(id);
+
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserUpdateViewModel model = new UserUpdateViewModel();
+            model.mail = values.Email;
+            model.namesurname = values.NameSurname;
+            model.imageurl = values.ImageUrl;
+            model.username = values.UserName;
+            return View(model);
         }
         [HttpPost]
-        public IActionResult WriterEditProfile(Writer p,string WriterPasswordConfirm)
+        public async Task<IActionResult>  WriterEditProfile(UserUpdateViewModel model)
         {
-            WriterValidator wl = new WriterValidator();
-            ValidationResult result = wl.Validate(p);
-            if (result.IsValid)
-            {
-                if (p.WriterPassword == WriterPasswordConfirm)
-                {
-                    wm.TUpdate(p);
-                    return RedirectToAction("WriterEditProfile", "Writer");
-                } 
-            }
-            else
-            {
-                foreach (var item in result.Errors)
-                {
-                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-                }
-                
-            }
-            ViewBag.Cities = new SelectList(cm.GetList().ToList(), "CityId", "CityName");
-            return View();
-
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            values.NameSurname = model.namesurname;
+            values.ImageUrl = model.imageurl;
+            values.Email = model.mail;
+            var result = await _userManager.UpdateAsync(values);
+            return RedirectToAction("WriterEditProfile", "Writer");
         }
 
         [AllowAnonymous]
