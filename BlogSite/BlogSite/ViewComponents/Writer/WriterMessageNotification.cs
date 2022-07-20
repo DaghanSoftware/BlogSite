af82fe75572021b821d554100ec7a68d216006ec
Semih Dağhan <daghanex@hotmail.com>
2022-07-20 11:42:50 +0300

    Admin panelinde düzenlemeler.
    -Sayfalara yönlendirme linkleri verildi
    -Gelen Mesajlar Sayfası Oluşturuldu

--- BlogSite/BlogSite/ViewComponents/Writer/WriterMessageNotification.cs
+++ BlogSite/BlogSite/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,9 +14,13 @@ namespace BlogSite.ViewComponents.Writer
         MessageManager mm = new MessageManager(new EfMessageRepository());
         public IViewComponentResult Invoke()
         {
-            int p;
-            p= 1;
-            var values = mm.GetInboxListByWriter(p);
+            //int p;
+            //p= 1;
+            var username = User.Identity.Name;
+            Context c = new Context();
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var WriterId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(WriterId);
             return View(values);
         }
     }
