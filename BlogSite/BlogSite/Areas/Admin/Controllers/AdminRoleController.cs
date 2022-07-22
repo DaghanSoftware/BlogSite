7569f3c5d2230fa4aba24921d8303a0bd6e3475c
Semih Dağhan <daghanex@hotmail.com>
2022-07-22 11:50:39 +0300

    Role güncelleme ve silme işlemleri tamamlandı

--- BlogSite/BlogSite/Areas/Admin/Controllers/AdminRoleController.cs
+++ BlogSite/BlogSite/Areas/Admin/Controllers/AdminRoleController.cs
@@ -52,5 +52,41 @@ namespace BlogSite.Areas.Admin.Controllers
             };
             return View(p);
         }
+
+        [HttpGet]
+        public IActionResult UpdateRole(int id)
+        {
+            var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            RoleUpdateViewModel model = new RoleUpdateViewModel
+            {
+                Id = values.Id,
+                Name = values.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>  UpdateRole(RoleUpdateViewModel model)
+        {
+            var values = _roleManager.Roles.Where(x => x.Id == model.Id).FirstOrDefault();
+            values.Name = model.Name;
+            var result = await _roleManager.UpdateAsync(values);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var values = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            var result = await _roleManager.DeleteAsync(values);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
