3917a57ef71af5b96b13b0adb997626bf7a4b73c
Semih Dağhan <daghanex@hotmail.com>
2022-07-22 13:21:48 +0300

    Kullanıcılara toplu olarak rol atama ve rol kaldırma işlemi

--- BlogSite/BlogSite/Areas/Admin/Controllers/AdminRoleController.cs
+++ BlogSite/BlogSite/Areas/Admin/Controllers/AdminRoleController.cs
@@ -13,10 +13,12 @@ namespace BlogSite.Areas.Admin.Controllers
     public class AdminRoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AdminRoleController(RoleManager<AppRole> roleManager)
+        public AdminRoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -87,6 +89,52 @@ namespace BlogSite.Areas.Admin.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+        public IActionResult UserRoleList()
+        {
+            var values = _userManager.Users.ToList();
+
+            return View(values);
+        }
+        [HttpGet]
+        public async Task<IActionResult>  AssignRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            var roles = _roleManager.Roles.ToList();
+
+            TempData["Userid"] = user.Id;
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> model = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel m = new RoleAssignViewModel();
+                m.RoleID = item.Id;
+                m.Name = item.Name;
+                m.Exists = userRoles.Contains(item.Name);
+                model.Add(m);
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(List<RoleAssignViewModel> model)
+        {
+            var userid = (int)TempData["UserId"];
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == userid);
+            foreach (var item in model)
+            {
+                if (item.Exists)
+                {
+                    await _userManager.AddToRoleAsync(user, item.Name);
+                }
+                else
+                {
+                    await _userManager.RemoveFromRoleAsync(user, item.Name);
+                }
+                
+            }
+            
+;            return RedirectToAction("UserRoleList");
         }
     }
 }
